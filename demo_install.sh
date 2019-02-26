5b92665b4c9f1d2fb53b9dc3d55c4b49952518bc
bertolinocastro <jgabbc@hotmail.com>
2019-02-26 18:23:18 -0300

    Added help messages, python version checking.

--- demo_install.sh
+++ demo_install.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
+printf "
+Automated demo installer.
+Is uses python's 3 or higher venv module.
 
-PYTHON=python3
+It must be sourced, not directly executed. For instance: source ./demo_install.sh
 
-# automated install script
+You may pass non environment python executable as inline var.
+For instance: PYTHON=python3.7 source ./demo_install.sh
 
-$PYTHON -c << EOF
+"
+
+if [[ -z $PYTHON ]]; then
+  PYTHON=python3
+fi
+
+echo -e "Starting virtual environment creation...\n"
+
+$PYTHON -c "
 import sys
-sys.exit(sys.version_info < (3.7))
-EOF
+sys.exit(sys.version_info < (3,7))
+"
 
-echo $?
+if [[ $? -eq 1 ]];then
+  echo "Wrong python version. It must be 3.7 or higher."
+  echo "Alternatively, you can set inline PYTHON var. For example: PYTHON=python3.7 source ./demo_install.sh"
+else
+  version=$($PYTHON -c 'import sys;print("%s.%s"%sys.version_info[0:2])')
+  dpkg -s python${version}-venv > /dev/null
+  if [[ 1 -eq $? ]];then
+    echo "python${version}-venv package not found!"
+    echo "You must install it or a higher version"
+  else
+    $PYTHON -m venv venv
+    echo "Python virtual environment created."
+    . venv/bin/activate
+    echo -e "Installing Logbuch in actual virtual environment...\n"
+    pip3 install --editable .
+  fi
 
-# demo install using pyenv
-sudo apt install python3.7-venv
-python3.7 -m venv venv
-
-. teste_env/bin/activate
-pip3 install --editable .
+fi
