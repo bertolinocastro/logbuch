3ee09f779b7ab753f3482c3589004fa61d063ef0
bertolinocastro <jgabbc@hotmail.com>
2019-02-25 22:47:39 -0300

    Minor changes.

--- logbuch/src/logbuch/make.py
+++ logbuch/src/logbuch/make.py
@@ -1,6 +1,6 @@
 from .md2tex import Md2Tex
 
-def make(conf):
+def make(conf,proj):
 
     # TODO: ---v
     # converting each markdown file contents to latex ones
@@ -8,4 +8,4 @@ def make(conf):
     # Afterall, create the main latex file to import annd sections and images and
     # then compile it
 
-    print('make')
+    texFile = Md2Tex(conf,proj)
