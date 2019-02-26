90b55c873cbd95ddd0473820bfe57b6c44a811c3
bertolinocastro <jgabbc@hotmail.com>
2019-02-26 15:16:17 -0300

    Created deleting procedures.

--- logbuch/src/logbuch/projpromp.py
+++ logbuch/src/logbuch/projpromp.py
@@ -1,18 +1,52 @@
 import os
+import shutil
+import sys
+import click
 
-def prompProj(config):
+def prompProj(config,proj):
     path = config.projsDir()
     act = config.actProj()
 
     # listing topics inside active or all projects
     lis = sorted(listDir(path))
-    printTree(lis,path,act)
 
-    # TODO: Create an feature to create a project("topics")
+    if proj:
+        if proj in lis:
+            print('Switching to existing project "%s"\n'%proj)
+        else:
+            print('Creating new project "%s" and setting as active\n'%proj)
+            createProj(path,proj)
+            lis = sorted(lis+[proj]) # appending regarding the order
+        config.setActive(proj)
+        # printing projects tree
+        printTree(lis,path,proj,numbers=False)
+    else:
+        # printing projects tree
+        printTree(lis,path,act)
+        res = promptAns(lis)
+        if res == -1:
+            sys.exit()
+        elif res == -2:
+            print('\nEntered delete mode!')
+            res = promptAns(lis,remove=True)
+            if res == -1:
+                sys.exit()
+            if click.confirm('\nProject "%s" is going to be deleted. Are you sure?'%lis[res]):
+                delProj(path,lis[res])
+                print('Deleted.')
+                lis.remove(res)
+                if lis[res] == act:
+                    print('Switching to %s\n'%lis[0])
+                    config.setActive(lis[0])
+        else:
+            print('Switching to %s\n'%lis[res])
+            config.setActive(lis[res])
 
-    res = promptAns(lis)
-    print('Switching to %s\n'%lis[res])
-    config.setActive(lis[res])
+def createProj(path,proj):
+    os.mkdir(path+'/'+proj)
+
+def delProj(path,proj):
+    shutil.rmtree(path+'/'+proj)
 
 def listDir(path):
     if not os.path.exists(path):
@@ -23,7 +57,7 @@ def listDir(path):
     return projs
 
 # it prints the entire found project tree
-def printTree(lis,path,active):
+def printTree(lis,path,active,numbers=True):
     basenm = os.path.basename(path)
 
     left_padd = ' '*(len(basenm)+1)
@@ -32,21 +66,31 @@ def printTree(lis,path,active):
     lth = max([len(x) for x in lis])
     for j,key in enumerate(lis):
         isAct = ' * ' if key == active else ' '*3
+        app_num = str(j+1) if numbers else ''
 
         if len(lis) <= 1:
-            left_bar = chr(9472)
+            left_bar = chr(9492)
         elif j == len(lis) -1: # last row
             left_bar = chr(9492)
         else: # all other rows
             left_bar = chr(9500)
-        print(left_padd+left_bar+chr(9472)*8+isAct+'%s'%key+' '*(lth-len(key)+1)+str(j+1))
+        print(left_padd+left_bar+chr(9472)*8+isAct+'%s'%key+' '*(lth-len(key)+1)+app_num)
     print()
 
-def promptAns(lis):
-    msg = 'Choose a project number to activate: '
-    res = inputNumber(msg)-1
-    while res not in list(range(len(lis))):
-        res = inputNumber('Wrong choice. It must be between %d and %d\n'%(1, len(lis))+msg)-1
+def promptAns(lis,remove=False):
+    padd = (' '*(2+len(str(len(lis)))))
+    if not remove:
+        msg = ' [1~%d]: make project active\n'%len(lis) +\
+            ' %s-1: asks a project to delete\n'%padd+\
+            ' %s 0: exits gracefully\n'%padd
+    else:
+        msg = ' [1~%d]: delete project\n'%len(lis)+\
+            ' %s 0: exits gracefully\n'%padd
+    msg1 = 'Choose a number: '
+    opts = list(range(-2,len(lis))) if not remove else list(range(-1,len(lis)))
+    res = inputNumber(msg+msg1)-1
+    while res not in opts:
+        res = inputNumber('\nWrong choice. It must be one of:\n%s\n%s'%(msg,msg1))-1
     return res
 
 def inputNumber(message):
