7fa2f063d840cf0d219473a63926d8c2d5158593
bertolinocastro <jgabbc@hotmail.com>
2019-02-27 02:14:34 -0300

    Finalized install script.

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+printf "
+Automated installer.
+It uses python 3.7 or higher.
 
-PYTHON=python3
+You may pass python executable as inline var.
+For instance: PYTHON=python3.7 ./install.sh
 
-# automated install script
+"
+if [[ -z $PYTHON ]]; then
+  PYTHON=python3
+fi
 
-$PYTHON -c << EOF
+$PYTHON -c "
 import sys
-sys.exit(sys.version_info < (3.7))
-EOF
-
-echo $?
+sys.exit(sys.version_info < (3,7))
+"
+if [[ $? -eq 1 ]];then
+  echo "Wrong python version. It must be 3.7 or higher."
+  echo "Alternatively, you can set inline PYTHON var. For example: PYTHON=python3.7 ./install.sh"
+else
+  version=$($PYTHON -c 'import sys;print("%s.%s"%sys.version_info[0:2])')
+  echo -e "Installing Logbuch...\n"
+  python${version} -m pip install .
+fi
