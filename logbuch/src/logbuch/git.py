35abc62b17a877cc69f6c0ccc248899848a1caab
bertolinocastro <jgabbc@hotmail.com>
2019-02-28 19:45:00 -0300

    Checking repository existence before any git command.

--- logbuch/src/logbuch/git.py
+++ logbuch/src/logbuch/git.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import subprocess
 import datetime
 
@@ -6,6 +8,11 @@ class Git:
         cwd = conf.projsDir()
         act = conf.actProj()
         now = datetime.datetime.now().strftime('%H:%M %d.%m.%Y')
+
+        # checking repository existence
+        if not _has_repository(cwd):
+            print('Cannot procede auto commiting. There is no repository at "%s".'%cwd)
+            return
 
         # adding file
         subprocess.run(['git','add',act+'/'+subj],cwd=cwd)
@@ -16,4 +23,19 @@ class Git:
 
     def wrapper(conf,args):
         cwd = conf.projsDir()
+
+        # checking repository existence
+        if not _has_repository(cwd,args):
+            print('Cannot procede with redirection. There is no repository at "%s".'%cwd)
+            sys.exit()
+
         subprocess.run(['git']+args,cwd=cwd)
+
+def _has_repository(path,args=[]):
+    if 'init' in args and os.path.exists(path): # allowing to create a repository
+        return True
+    try:
+        ret = subprocess.run(['git','status'],cwd=path,capture_output=True)
+        return ret.returncode == 0
+    except:
+        return False
