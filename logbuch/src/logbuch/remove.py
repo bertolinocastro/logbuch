2e136ef9096f97c50a9270183bda50e740a2cede
bertolinocastro <jgabbc@hotmail.com>
2019-03-08 02:28:29 -0300

    Calling the Topic delete mode and changes to outputs

--- logbuch/src/logbuch/remove.py
+++ logbuch/src/logbuch/remove.py
@@ -2,6 +2,7 @@ import os
 import sys
 import click
 import re
+from .topic import Topic
 
 def remove(file,config):
     if not file:
@@ -12,33 +13,27 @@ def remove(file,config):
     actv = config.actProj()
     ext  = config.getExt()
 
-    file = unheadarise(file)
+    # file = unheadarise(file)
 
     files = os.listdir(path+'/'+actv)
     if len(files) <= 0:
         print('Actual project is empty!\nUse "-l all" option to check other projects contents')
         sys.exit(0)
 
-    if os.path.exists(path+'/'+actv+'/'+file+ext):
-        print('\nSubject "%s" in project "%s" will be deleted.'%(headarise(file),actv))
-        print(fileInfo(path+'/'+actv+'/'+file+ext))
+    if os.path.exists(path+'/'+actv+'/'+unheadarise(file)+ext):
+        print('\nSubject "%s" in project "%s" will be deleted.'%(headarise(file),headarise(actv)))
+        top = Topic(file,config)
+        print(fileInfo(top.getFileContents()))
         if click.confirm('Are you sure?',abort=True):
-            os.remove(path+'/'+actv+'/'+file+ext)
+            top.delete()
     else:
         print('This subject doest not exist in "%s" project!\nUse -l option to check existing subjects'%actv)
 
-# TODO: rewrite this function using Topic class
-def fileInfo(file):
-    with open(file,'r') as f:
-        content = f.read()
-        try:
-            res  = '\nDate of creation: %s\n'%re.findall('Date: (\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2})',content)[0]
-            res += 'Contents: %d lines'%len(re.findall('\n',content))
-            res += ' and %d characters\n'%len(content)
-        except:
-            res = '\nCould not read file contents! You shall see its contents before deleting it.\n'
-        return res
-    return ''
+def fileInfo(dic):
+    res  = '\nDate of creation: %s\n'%dic['header']['date']
+    res += 'Contents: %d lines'%len(dic['text'].splitlines())
+    res += ' and %d characters\n'%len(dic['text'])
+    return res
 
 def unheadarise(s):
     return '_'.join(s.lower().split(' '))
